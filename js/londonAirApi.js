2b80e098a137ea1a78f9d7f8351baa837a10e7b8
Clemens Anzmann <clemens.anzmann@googlemail.com>
2018-12-21 14:56:06 +0100

    update handling of api data

--- js/londonAirApi.js
+++ js/londonAirApi.js
@@ -35,18 +35,20 @@ var getLondonAirQuality = function (siteCode, callback) {
           if (response && response.HourlyAirQualityIndex) {
             console.log(response);
             if (response.HourlyAirQualityIndex.LocalAuthority && response.HourlyAirQualityIndex.LocalAuthority.Site && response.HourlyAirQualityIndex.LocalAuthority.Site.species) {
-              var dioxideData = response.HourlyAirQualityIndex.LocalAuthority.Site.species[0];
               var locationName = response.HourlyAirQualityIndex.LocalAuthority['@LocalAuthorityName'];
-              var speciesName = dioxideData['@SpeciesName'];
-              var airQualityBand = dioxideData['@AirQualityBand'];
-              var airQualityIndex = dioxideData['@AirQualityIndex'];
-              if(airQualityBand && airQualityIndex && airQualityBand != 'No data') {
-                displayFooter(locationName, speciesName, airQualityBand, getLondonAirApiUrl(siteCode));
-                callback(airQualityIndex*2);
-              }
-              else {
-                console.log('London Air Api responsed with "No Data".');
-              }
+              var airQualityDataArray = response.HourlyAirQualityIndex.LocalAuthority.Site.species;
+              var validDataFound = false;
+              airQualityDataArray.forEach(function(airQualityDataPoint) {
+                var speciesName = airQualityDataPoint['@SpeciesName'];
+                var airQualityBand = airQualityDataPoint['@AirQualityBand'];
+                var airQualityIndex = parseInt(airQualityDataPoint['@AirQualityIndex']);
+                console.log(speciesName + ' in London Air Api is ' + airQualityBand);
+                if (!validDataFound && airQualityBand && airQualityIndex && airQualityBand != 'No data') {
+                  displayFooter(locationName, speciesName, airQualityBand, getLondonAirApiUrl(siteCode));
+                  validDataFound = true;
+                  callback(airQualityIndex);
+                }
+              });
             } else {
               console.log('London Air Api response does not contain expected data.');
             }
