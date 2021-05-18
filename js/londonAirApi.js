5ece8838cdf7361484b7b143f0be131279b23ac3
Clemens Anzmann <clemens.anzmann@gmail.com>
2021-05-18 10:35:17 +0100

    maintain London Api api datavis

--- js/londonAirApi.js
+++ js/londonAirApi.js
@@ -7,6 +7,9 @@ var getLondonAirApiUrl = function (siteCode) {
 };
 
 var getLondonTimeSeriesAirApiUrl = function (siteCode) {
+  // use local data until API connection is fixed
+  return '/js/localPollutionData.json';
+
   var date = new Date();
   var day, month, year;
 
@@ -43,7 +46,7 @@ var displayElement = function (id) {
   var element = document.getElementById(id);
   if (element && element.style) {
     element.style.opacity = '1.0';
-    element.style.display = 'inline';
+    element.style.display = 'block';
   }
 };
 
@@ -58,102 +61,100 @@ var displayFooterData = function (locationName, speciesName, airQualityBand, url
 
 var getLondonAirQuality = function (siteCode, callback) {
   var xhr = new XMLHttpRequest();
-  try {
-    xhr.open('GET', getLondonAirApiUrl(siteCode), true);
-    xhr.onload = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          try {
-            var response = JSON.parse(xhr.response);
-          } catch (err) {
-            console.log('Unable to parse London Air API response: ' + err);
-            callback(10);
-          }
-          if (response && response.HourlyAirQualityIndex) {
-            console.log('Current Hour', response);
-            if (response.HourlyAirQualityIndex.LocalAuthority
-              && response.HourlyAirQualityIndex.LocalAuthority.Site
-              && response.HourlyAirQualityIndex.LocalAuthority.Site.species) {
-              var locationName = response.HourlyAirQualityIndex.LocalAuthority['@LocalAuthorityName'];
-              var airQualityDataArray = response.HourlyAirQualityIndex.LocalAuthority.Site.species;
-              var validDataFound = false;
-              airQualityDataArray.forEach(function(airQualityDataPoint) {
-                var speciesName = airQualityDataPoint['@SpeciesName'];
-                var airQualityBand = airQualityDataPoint['@AirQualityBand'];
-                var airQualityIndex = parseInt(airQualityDataPoint['@AirQualityIndex']);
-                console.log(speciesName + ' in London Air Api is ' + airQualityBand);
-                if (!validDataFound && airQualityBand && airQualityIndex && airQualityBand != 'No data') {
-                  displayFooter();
-                  displayFooterData(locationName, speciesName, airQualityBand, getLondonAirApiUrl(siteCode));
-                  displayElement('footer-point-expl');
-                  validDataFound = true;
-                  callback(airQualityIndex);
-                }
-              });
-            } else {
-              console.log('London Air Api response does not contain expected data.');
-            }
+  xhr.open('GET', getLondonAirApiUrl(siteCode), true);
+  xhr.onload = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        try {
+          var response = JSON.parse(xhr.response);
+        } catch (err) {
+          console.log('Unable to parse London Air API response: ', err);
+          callback();
+        }
+        if (response && response.HourlyAirQualityIndex) {
+          if (response.HourlyAirQualityIndex.LocalAuthority
+            && response.HourlyAirQualityIndex.LocalAuthority.Site
+            && response.HourlyAirQualityIndex.LocalAuthority.Site.species) {
+            var locationName = response.HourlyAirQualityIndex.LocalAuthority['@LocalAuthorityName'];
+            var airQualityDataArray = response.HourlyAirQualityIndex.LocalAuthority.Site.species;
+            var validDataFound = false;
+            airQualityDataArray.forEach(function(airQualityDataPoint) {
+              var speciesName = airQualityDataPoint['@SpeciesName'];
+              var airQualityBand = airQualityDataPoint['@AirQualityBand'];
+              var airQualityIndex = parseInt(airQualityDataPoint['@AirQualityIndex']);
+              console.log(speciesName + ' in London Air Api is ' + airQualityBand);
+              if (!validDataFound && airQualityBand && airQualityIndex && airQualityBand != 'No data') {
+                displayElement('footer');
+                displayElement('footer-text');
+                displayElement('footer-point-expl');
+                displayFooterData(locationName, speciesName, airQualityBand, getLondonAirApiUrl(siteCode));
+                validDataFound = true;
+                callback(airQualityIndex);
+              }
+            });
           } else {
-            console.log('London Air Api response does not contain expected data.');
+            console.error('London Air Api response does not contain expected data.');
           }
+        } else {
+          console.error('London Air Api response does not contain expected data.');
         }
       }
-    };
-    xhr.send(null);
-  } catch (err) {
-    console.log('Unable to retrieve London Air API data: ' + err);
+    }
+  };
+  xhr.send(null);
+  xhr.onerror = function (err) {
+    console.error('Unable to retrieve London Air API data: ', err);
   }
 };
 
 var getLondonTimeSeriesAirQuality = function (siteCode, callback) {
   var xhr = new XMLHttpRequest();
-  console.log(getLondonTimeSeriesAirApiUrl(siteCode))
-  try {
-    xhr.open('GET', getLondonTimeSeriesAirApiUrl(siteCode), true);
-    xhr.onload = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          try {
-            var response = JSON.parse(xhr.response);
-          } catch (err) {
-            console.log('Unable to parse London Air API response: ' + err);
-            callback(10);
-          }
-          if (response && response.AirQualityData) {
-            if (response.AirQualityData.RawAQData
-              && response.AirQualityData.RawAQData.Data
-              && response.AirQualityData.RawAQData.Data.length > 0
-              && response.AirQualityData.Columns
-              && response.AirQualityData.Columns.Column
-              && response.AirQualityData.Columns.Column .length > 0) {
-              console.log('Last Week Time Series:', response);
-              var timeSeriesArray = [];
-              var metricNameArray = [];
-              response.AirQualityData.Columns.Column.forEach(function(column) {
-                if (column && column['@ColumnName'] && column['@ColumnId']) {
-                  metricNameArray.push(column['@ColumnName'].split(': ')[1]);
-                  var timeSeries = response.AirQualityData.RawAQData.Data.map(function(record = {}) {
-                    return parseFloat(record['@' + column['@ColumnId']])
-                  })
-                  timeSeriesArray.push(
-                    timeSeries.filter(function(d) { return !isNaN(d) })
-                  )
-                }
-              })
-              displayFooter();
-              displayElement('footer-line-expl');
-              callback({ timeSeriesArray, metricNameArray });
-            } else {
-              console.log('London Air Api response does not contain expected data.');
-            }
+  xhr.open('GET', getLondonTimeSeriesAirApiUrl(siteCode), true);
+  xhr.onload = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        try {
+          var response = JSON.parse(xhr.response);
+        } catch (err) {
+          console.error('Unable to parse London Air API response: ', err);
+          callback();
+        }
+        if (response && response.AirQualityData) {
+          if (response.AirQualityData.RawAQData
+            && response.AirQualityData.RawAQData.Data
+            && response.AirQualityData.RawAQData.Data.length > 0
+            && response.AirQualityData.Columns
+            && response.AirQualityData.Columns.Column
+            && response.AirQualityData.Columns.Column .length > 0) {
+            console.log('Pollution Time Series:', response);
+            var timeSeriesArray = [];
+            var metricNameArray = [];
+            response.AirQualityData.Columns.Column.forEach(function(column) {
+              if (column && column['@ColumnName'] && column['@ColumnId']) {
+                metricNameArray.push(column['@ColumnName'].split(': ')[1]);
+                var timeSeries = response.AirQualityData.RawAQData.Data.map(function(record = {}) {
+                  return parseFloat(record['@' + column['@ColumnId']])
+                })
+                timeSeriesArray.push(
+                  timeSeries.filter(function(d) { return !isNaN(d) })
+                )
+              }
+            })
+            displayElement('footer');
+            displayElement('footer-text');
+            displayElement('footer-line-expl');
+            callback({ timeSeriesArray, metricNameArray });
           } else {
-            console.log('London Air Api response does not contain expected data.');
+            console.error('London Air Api response does not contain expected data.');
           }
+        } else {
+          console.error('London Air Api response does not contain expected data.');
         }
       }
-    };
-    xhr.send(null);
-  } catch (err) {
-    console.log('Unable to retrieve London Air API data: ' + err);
+    }
+  };
+  xhr.send(null);
+  xhr.onerror = function (err) {
+    console.error('Unable to retrieve London Air API data: ', err);
   }
 };
